7ad62614b939abade1f8e4fbcabfcb9589c02d54
Mikhail Yurasov <me@yurasov.me>
2021-05-10 21:27:40 -0700

    Cleanup after uploading repo to kaggle

--- kaggle/upload_repo.sh
+++ kaggle/upload_repo.sh
@@ -3,6 +3,7 @@
 # upload code as kaggle dataset
 
 DATASET_ID="bc21-code"
+GIT_URL="git@github.com:myurasov/Kaggle-BirdCLEF-2021.git"
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 REPO_DIR="${THIS_DIR}/__repo__"
@@ -11,7 +12,7 @@ rm -rf "${REPO_DIR}"
 mkdir -pv "${REPO_DIR}"
 
 cd "${REPO_DIR}"
-git clone git@github.com:myurasov/Kaggle-BirdCLEF-2021.git .
+git clone "${GIT_URL}" .
 rm -rf .git
 
 kaggle datasets init
@@ -21,3 +22,5 @@ sed -i "s/INSERT_SLUG_HERE/bc21-code/" dataset-metadata.json
 # second option should be used afer initial creation
 # kaggle datasets create --dir-mode zip
 kaggle datasets version --dir-mode zip -m "`date -u | sed 's/[ :]/_/g'`"
+
+rm -rf "${REPO_DIR}"
