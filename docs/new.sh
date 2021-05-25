b7a98093141bc3228bbc7703d85cab53f017afa5
Mikhail Yurasov <me@yurasov.me>
2021-05-25 14:59:16 -0700

    Update

--- docs/new.sh
+++ docs/new.sh
@@ -1,9 +1,7 @@
 rm -rf _data _work ; cli/download_data.sh && \
 jupyter nbconvert --to notebook --execute notebooks/convert_n_data.ipynb && \
-cli/prepare_short.run.py --min_rating 3 --max_from_clip 10 --no_rating_value 3 --rectify_class_balance 0 --sample_with_stride 5 --out_csv short-D.csv && \
-cli/prepare_long.run.py --split_multilabel 1 --out_csv long-D.csv && \
-cli/create_dataset.run.py --in_csvs short-D.csv long-D.csv --out dataset-D.pickle --secondary_label_p 0.33 --rectify_class_balance 2.5 0.25 --geofilter all-500mi-last_5y-1mo_tolerance --folds 7 && \
-cli/cache_waves.py --dataset dataset-D.pickle && \
+
+
 cli/prepare_short.run.py --min_rating 3 --max_from_clip 0 --no_rating_value 3 --rectify_class_balance 0 --sample_with_stride 5 --sample_with_detection_csv /app/res/n_nocall_predictions.csv.gz --out_csv short-F.csv && \
 cli/prepare_long.run.py --split_multilabel 1 --out_csv long-F.csv && \
 cli/create_dataset.run.py --in_csvs n_nocall.csv short-F.csv long-F.csv --out dataset-F.pickle --secondary_label_p 0.33 --rectify_class_balance 2.5 0.25 --geofilter all-500mi-last_5y-1mo_tolerance --folds 7 && \
